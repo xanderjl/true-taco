9e9f934e9629e6aff761bef860e6e9d140a6f6da
XanderJL <alexanderjameslow@gmail.com>
2020-09-06 17:29:43 -0400

    added: global theme

---
+++ site/styles/theme.js
@@ -0,0 +1,25 @@
+export default {
+  breakpoints: [
+    { tablet: "769px" },
+    { desktop: "1024px" },
+    { widescreen: "1216px" },
+    { fullhd: "1408" },
+  ],
+  fonts: {
+    heading: "'Sunset Boulevard', sans-serif",
+    banner: "'York White Letter', sans-serif",
+    body: "'Lato', sans-serif",
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "4rem",
+  },
+}

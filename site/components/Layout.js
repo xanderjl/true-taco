e078f96609dba0981743082e6e58feafc16bd882
XanderJL <alexanderjameslow@gmail.com>
2020-09-13 19:49:54 -0400

    refactored: removed unused vars and added types

--- site/components/Layout.js
+++ site/components/Layout.js
@@ -1,18 +1,22 @@
-import React from "react"
+import PropTypes from "prop-types"
 import Head from "next/head"
 import Navbar from "../components/Navbar"
 
-const Layout = ({ children }) => {
+const Layout = ({ title, children }) => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>{title ? title + " | " : null}True Taco</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Navbar />
       {children}
     </div>
   )
+}
+
+Layout.propTypes = {
+  title: PropTypes.string,
 }
 
 export default Layout

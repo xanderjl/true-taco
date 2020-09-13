2263e9d7524886f09fb46d8d4c250d4694507cf3
XanderJL <alexanderjameslow@gmail.com>
2020-09-13 19:49:01 -0400

    added: container component for nav items

--- site/components/Navbar.js
+++ site/components/Navbar.js
@@ -1,38 +1,43 @@
-import React from "react"
-import NavbarItem from "../components/NavbarItem"
+import Link from "next/link"
+import { Box, Flex, Icon, Image } from "@chakra-ui/core"
 import customTheme from "../styles/theme"
-import { Image, Flex } from "@chakra-ui/core"
+import NavbarItem from "../components/NavbarItem"
+import Container from "../components/Container"
 
 const Navbar = () => {
   const { colors } = customTheme
   return (
-    <div>
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        bg={colors.green.light}
-        p="0 0.75rem"
-      >
-        <Flex alignItems="center" justifyContent="space-around">
-          <NavbarItem>Home</NavbarItem>
-          <NavbarItem>About Us</NavbarItem>
-          <NavbarItem>Menu</NavbarItem>
-          <NavbarItem>Contact</NavbarItem>
+    <Box bg={colors.green.light} p="0 0.75rem">
+      <Container>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center" justifyContent="space-around">
+            <NavbarItem href="/">Home</NavbarItem>
+            <NavbarItem>About Us</NavbarItem>
+            <NavbarItem>Menu</NavbarItem>
+            <NavbarItem>Contact</NavbarItem>
+          </Flex>
+          <NavbarItem>
+            Takeout (n){" "}
+            <Icon name="brownBag" w="auto" height="2rem" ml="0.75rem" />
+          </NavbarItem>
         </Flex>
-        <NavbarItem>Takeout (n)</NavbarItem>
-      </Flex>
-      <Image
-        zIndex="1"
-        w="6rem"
-        h="auto"
-        src="/logo/2.svg"
-        alt="True Taco logo"
-        pos="absolute"
-        top="1rem"
-        left="50%"
-        transform="translateX(-25%)"
-      />
-    </div>
+      </Container>
+      <Link href="/">
+        <a>
+          <Image
+            zIndex="1"
+            w="6rem"
+            h="auto"
+            src="/logo/2.svg"
+            alt="True Taco logo"
+            pos="absolute"
+            top="1rem"
+            left="50%"
+            transform="translateX(-25%)"
+          />
+        </a>
+      </Link>
+    </Box>
   )
 }
 

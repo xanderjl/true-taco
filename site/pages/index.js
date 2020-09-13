595a5aadbabece9976ba718bb2ceaaf2d098d916
XanderJL <alexanderjameslow@gmail.com>
2020-09-13 19:49:38 -0400

    refactored: moved menu into its own component

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,12 +1,13 @@
-import React from "react"
 import Layout from "../components/Layout"
-import { Box, Heading, Flex, Grid, Text } from "@chakra-ui/core"
+import { Box, Heading } from "@chakra-ui/core"
 import customTheme from "../styles/theme"
+import Menu from "../components/Menu"
+import Container from "../components/Container"
 
 const Home = () => {
-  const { colors, fonts } = customTheme
+  const { colors, maxWidth } = customTheme
   return (
-    <Layout>
+    <Layout title="Home">
       <Box
         w="100%"
         h="800px"
@@ -17,61 +18,24 @@ const Home = () => {
         backgroundSize="cover"
         backgroundRepeat="none"
       >
-        <Heading
-          d="inline-block"
-          as="h1"
-          p="0.25rem 1.25rem"
-          pos="absolute"
-          bottom="15%"
-          right="12%"
-          bg={colors.red.light}
-          color={colors.white}
-          fontWeight="400"
-          fontSize="6xl"
-        >
-          authentic comedor latino
-        </Heading>
+        <Container pos="relative">
+          <Heading
+            d="inline-block"
+            as="h1"
+            p="0.25rem 1.25rem"
+            pos="absolute"
+            bottom={["0", "15%", "15%", "15%"]}
+            right="0"
+            bg={colors.red.light}
+            color={colors.white}
+            fontWeight="400"
+            fontSize="6xl"
+          >
+            authentic comedor latino
+          </Heading>
+        </Container>
       </Box>
-      <Box p="3rem 1.25rem" bg={colors.black}>
-        <Heading
-          mb="3rem"
-          as="h1"
-          color={colors.white}
-          fontWeight="400"
-          fontSize="6xl"
-          textAlign="center"
-          textDecor="underline"
-        >
-          Saturday Pickup
-        </Heading>
-        <Grid gridTemplateColumns="repeat(2, 1fr)">
-          <Flex direction="column">
-            <Flex justifyContent="space-around">
-              <Heading
-                as="h2"
-                color={colors.white}
-                fontWeight="400"
-                fontSize="5xl"
-              >
-                Quesadillas
-              </Heading>
-              <Heading
-                as="h2"
-                color={colors.white}
-                fontFamily={fonts.banner}
-                fontWeight="400"
-                fontSize="5xl"
-              >
-                Õ3.5-7Ô
-              </Heading>
-            </Flex>
-            <Text color={colors.white}>
-              Mixed cheeses, tomatoes, green onions. Choose your filling. Choose
-              your shell. Served with sour cream and sour cream.
-            </Text>
-          </Flex>
-        </Grid>
-      </Box>
+      <Menu p="8rem 1.25rem" m="auto" />
     </Layout>
   )
 }

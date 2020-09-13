43ef5b4be141c9adbd03408d57ba983613c5bb4a
XanderJL <alexanderjameslow@gmail.com>
2020-09-13 19:48:15 -0400

    refactored: created component for each menu item

---
+++ site/components/MenuItem.js
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core"
+import { Flex, Heading, Text } from "@chakra-ui/core"
+import customTheme from "../styles/theme"
+
+const MenuItem = ({ heading, price, children }) => {
+  const { colors, fonts } = customTheme
+
+  const dotLeaders = css`
+    &::after {
+      float: right;
+      width: 0;
+      margin-left: 1.25rem;
+      white-space: nowrap;
+      content: ". . . . . . . . . . . . . . . . . . . ";
+    }
+  `
+
+  return (
+    <Flex direction="column">
+      <Flex
+        maxW="40em"
+        overflowX="hidden"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Heading
+          css={dotLeaders}
+          as="h2"
+          color={colors.white}
+          fontWeight="400"
+          fontSize="5xl"
+        >
+          {heading}
+        </Heading>
+        <Heading
+          d="inline-block"
+          as="h2"
+          pl="1.5rem"
+          color={colors.white}
+          bg={colors.black}
+          fontFamily={fonts.banner}
+          fontWeight="400"
+          fontSize="4xl"
+        >
+          Õ{price}Ô
+        </Heading>
+      </Flex>
+      <Text maxW="75%" color={colors.white}>
+        {children}
+      </Text>
+    </Flex>
+  )
+}
+
+export default MenuItem

88f5f4bce14e2936db2a8b8fba7434e87618904b
XanderJL <alexanderjameslow@gmail.com>
2020-10-02 23:53:18 -0400

    chore: complete mobile styling

--- site/src/components/Navbar.js
+++ site/src/components/Navbar.js
@@ -14,15 +14,24 @@ const Navbar = () => {
   return (
     <Box minH="72px" bg={colors.green.light} p="0 0.75rem">
       <Container>
-        <Flex alignItems="center" justifyContent="space-between">
-          <Flex alignItems="center" justifyContent="space-around">
-            <NavbarItem href="/">Home</NavbarItem>
-            <NavbarItem href="/catering">Catering</NavbarItem>
-          </Flex>
-          <NavbarItem>
-            <CartPopover />
-          </NavbarItem>
-          <Box d={["inline-block", "none"]}>
+        <Flex
+          alignItems={["flex-start", "center"]}
+          justifyContent="space-between"
+        >
+          <Box d={[!isOpen && `none`, "inline-block"]}>
+            <Flex
+              direction={["column", "row"]}
+              alignItems="center"
+              justifyContent="space-around"
+            >
+              <NavbarItem href="/">Home</NavbarItem>
+              <NavbarItem href="/catering">Catering</NavbarItem>
+            </Flex>
+            <NavbarItem>
+              <CartPopover />
+            </NavbarItem>
+          </Box>
+          <Box d={["flex", "none"]} w="100%" h="100%" justifyContent="flex-end">
             <Hamburger
               toggle={setIsOpen}
               toggled={isOpen}
@@ -34,13 +43,13 @@ const Navbar = () => {
       <Link to="/">
         <Image
           zIndex="1"
-          w={["6rem", "8rem"]}
+          w={["3rem", "6rem", "8rem"]}
           h="auto"
           src={Logo}
           alt="True Taco logo"
           pos="absolute"
-          top={["1rem", ".75rem"]}
-          left="50%"
+          top={["0.5rem", "1rem", ".75rem"]}
+          left={["1.25rem", "50%"]}
           transform="translateX(-25%)"
         />
       </Link>

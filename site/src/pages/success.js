73e1bc73c4da906ea2edb6358adc70bacfe261cf
XanderJL <alexanderjameslow@gmail.com>
2020-10-03 22:25:44 -0400

    added: payment success page

---
+++ site/src/pages/success.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+const Success = () => {
+  return (
+    <Layout>
+      YEEHAW
+    </Layout>
+  )
+}
+
+export default Success

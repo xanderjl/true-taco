f9bd5f18163bd59e9b01de3a37f54b56e72b3efb
XanderJL <alexanderjameslow@gmail.com>
2020-10-07 03:25:38 -0400

    fixed: cart styling

--- site/src/pages/cart.js
+++ site/src/pages/cart.js
@@ -1,7 +1,10 @@
+/** @jsx jsx */
 import React from "react"
 import Layout, { Container, Section } from "../components/Layout"
-import { Text, Button, Box, Flex, Grid } from "@chakra-ui/core"
+import { Text, Button, Icon, Box, Flex, Grid, Divider } from "@chakra-ui/core"
+import { css, jsx } from "@emotion/core"
 import { useShoppingCart } from "use-shopping-cart"
+import Zigs from "../images/frills/clip-zigs.svg"
 
 const Cart = () => {
   const {
@@ -32,18 +35,41 @@ const Cart = () => {
 
     redirectToCheckout({ sessionId: response.sessionId })
   }
+
+  const ziggies = css`
+    overflow: hidden;
+
+    &::after {
+      width: calc(100% + 6rem);
+      height: 22px;
+      content: "";
+      display: block;
+      position: relative;
+      bottom: -3rem;
+      left: -2.5rem;
+      background-image: url(${Zigs});
+    }
+  `
+
   return (
     <Layout title="Cart" bg="red.50">
       <Container>
-        <Section>
-          <Grid
-            templateColumns={["minmax(0, 1fr)", "repeat(2, 1fr)"]}
-            gap="1rem"
-          >
-            {cartItems.map((item, i) => {
-              const { name, sku, currency, formattedValue, quantity } = item
-              return (
-                <>
+        <Section
+          css={ziggies}
+          maxW="960px"
+          m="0 auto"
+          p={["3rem 1.25rem", "3rem 2.5rem"]}
+          bg="white"
+        >
+          {cartItems.map((item, i) => {
+            const { name, sku, currency, formattedValue, quantity } = item
+            return (
+              <Box key={i}>
+                <Grid
+                  templateColumns={["minmax(0, 1fr)", "repeat(2, 1fr)"]}
+                  gap="2.5rem"
+                  pb="2rem"
+                >
                   <Flex align="center" justify="space-between">
                     <Text as="span">{name}</Text>
                     <Flex>
@@ -95,10 +121,11 @@ const Cart = () => {
                     </Box>
                     <Text>{formattedValue}</Text>
                   </Flex>
-                </>
-              )
-            })}
-          </Grid>
+                </Grid>
+                <Divider mb="2rem" />
+              </Box>
+            )
+          })}
           <Flex p="2rem 0 1rem 0" justify="flex-end" align="center">
             <Text>Cart Total: ${totalPrice / 100}</Text>
           </Flex>
@@ -118,95 +145,6 @@ const Cart = () => {
         </Section>
       </Container>
     </Layout>
-    //    <Layout>
-    //    <Container>
-    //      <Section>
-    //        {cartItems.map((item, i) => {
-    //          const { name, sku, currency, formattedValue, quantity } = item
-    //          return (
-    //            <Flex
-    //              key={i}
-    //              p=".5rem 0"
-    //              direction={["column", "row"]}
-    //              justify="space-between"
-    //            >
-    //              <Flex
-    //                w={["100%", "50%"]}
-    //                pb={["3rem", "0"]}
-    //                justify="space-between"
-    //              >
-    //                <Flex align="center">
-    //                  <Text as="span">{name}</Text>
-    //                  <Box p="0 1rem">
-    //                    <Button
-    //                      size="sm"
-    //                      p="0"
-    //                      color="white"
-    //                      bg="red.light"
-    //                      _hover={{ bg: "red.200" }}
-    //                      borderRadius="0"
-    //                      onClick={() => quantity > 1 && decrementItem(sku)}
-    //                    >
-    //                      -
-    //                    </Button>
-    //                  </Box>
-    //                  <Text as="span">{quantity}</Text>
-    //                  <Box p="0 1rem">
-    //                    <Button
-    //                      size="sm"
-    //                      p="0"
-    //                      color="white"
-    //                      bg="red.light"
-    //                      _hover={{ bg: "red.200" }}
-    //                      borderRadius="0"
-    //                      onClick={() => incrementItem(sku)}
-    //                    >
-    //                      +
-    //                    </Button>
-    //                  </Box>
-    //                </Flex>
-    //              </Flex>
-    //              <Flex w={["100%", "50%"]} justify="flex-end" align="center">
-    //                <Box pr={["1rem", "2rem"]}>
-    //                  <Button
-    //                    size="sm"
-    //                    color="white"
-    //                    bg="red.light"
-    //                    _hover={{ bg: "red.200" }}
-    //                    borderRadius="0"
-    //                    onClick={() => removeItem(sku)}
-    //                  >
-    //                    {cartItems > 1 ? (
-    //                      <span>Remove Items</span>
-    //                    ) : (
-    //                      <span>Remove Item</span>
-    //                    )}
-    //                  </Button>
-    //                </Box>
-    //                <Text>{formattedValue}</Text>
-    //              </Flex>
-    //            </Flex>
-    //          )
-    //        })}
-    //        <Flex p="2rem 0 1rem 0" justify="flex-end" align="center">
-    //          <Text>Cart Total: ${totalPrice / 100}</Text>
-    //        </Flex>
-    //        <Flex justify="flex-end">
-    //          <Button
-    //            mb="1rem"
-    //            bg="red.light"
-    //            color="white"
-    //            _hover={{ bg: "red.200" }}
-    //            borderRadius="0"
-    //            onClick={handleSubmit}
-    //            onKeyDown={e => e.key === "Enter" && handleSubmit()}
-    //          >
-    //            Proceed to Checkout
-    //          </Button>
-    //        </Flex>
-    //      </Section>
-    //    </Container>
-    //  </Layout>
   )
 }
 

308a6db0afbeb54a3cc011694847b2ea61d555f5
XanderJL <alexanderjameslow@gmail.com>
2020-10-06 20:59:49 -0400

    added: calling card

--- site/src/components/Footer.js
+++ site/src/components/Footer.js
@@ -13,15 +13,16 @@ import {
 } from "@chakra-ui/core"
 import { Container, Section } from "./Layout"
 import { FaFacebookSquare } from "react-icons/fa"
-import { FiInstagram } from "react-icons/fi"
+import { FiInstagram, FiExternalLink } from "react-icons/fi"
 import { HiLocationMarker } from "react-icons/hi"
 
 const Footer = () => {
   return (
     <Box bg="orange.100" color="orange.900">
       <Container>
-        <Section>
+        <Section pb="1rem">
           <Grid
+            pb="4rem"
             gridTemplateColumns={["minmax(0, 1fr)", "repeat(3, 1fr)"]}
             gap={["0.75rem", "1rem"]}
           >
@@ -56,10 +57,15 @@ const Footer = () => {
                 Contact Us
               </Heading>
               <List>
-                <Link href="https://www.google.com/maps/place/The+Market+at+Western+Fair+District/@42.9910852,-81.2232538,17z/data=!3m1!4b1!4m12!1m6!3m5!1s0x0:0x6671687381277e25!2sTrue+Taco!8m2!3d42.989806!4d-81.2205571!3m4!1s0x882ef277c9e3af8d:0xa26ae32aa0dc2d71!8m2!3d42.9910852!4d-81.2210651">
-                  <ListItem>
+                <Link
+                  href="https://www.google.com/maps/place/The+Market+at+Western+Fair+District/@42.9910852,-81.2232538,17z/data=!3m1!4b1!4m12!1m6!3m5!1s0x0:0x6671687381277e25!2sTrue+Taco!8m2!3d42.989806!4d-81.2205571!3m4!1s0x882ef277c9e3af8d:0xa26ae32aa0dc2d71!8m2!3d42.9910852!4d-81.2210651"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ListItem d="flex" alignItems="center">
                     <ListIcon icon={HiLocationMarker} />
                     900 King Street, N5W 2X7, London Ontario
+                    <Box as={FiExternalLink} ml="0.25rem" />
                   </ListItem>
                 </Link>{" "}
                 <Link href="tel:226-237-2573">
@@ -74,21 +80,46 @@ const Footer = () => {
                     admin@truetaco.ca
                   </ListItem>
                 </Link>{" "}
-                <Link href="https://www.facebook.com/truetaco/">
-                  <ListItem>
+                <Link
+                  href="https://www.facebook.com/truetaco/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ListItem d="flex" alignItems="center">
                     <ListIcon icon={FaFacebookSquare} />
                     True Taco
+                    <Box as={FiExternalLink} ml="0.25rem" />
                   </ListItem>
                 </Link>{" "}
-                <Link href="https://www.instagram.com/truetaco/">
-                  <ListItem>
+                <Link
+                  href="https://www.instagram.com/truetaco/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ListItem d="flex" alignItems="center">
                     <ListIcon icon={FiInstagram} />
                     @truetaco
+                    <Box as={FiExternalLink} ml="0.25rem" />
                   </ListItem>
                 </Link>
               </List>
             </Flex>
           </Grid>
+          <Flex justify="center">
+            Designed by
+            <Link
+              d="flex"
+              alignItems="center"
+              ml="0.25rem"
+              href="https://alexlow.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="red.600"
+            >
+              Alex Low
+              <Box ml="0.25rem" as={FiExternalLink} />
+            </Link>
+          </Flex>
         </Section>
       </Container>
     </Box>

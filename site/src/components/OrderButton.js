93053632b7fe3d20bb2ddef5f1b3edf49130d347
XanderJL <alexanderjameslow@gmail.com>
2020-10-09 12:34:34 -0400

    refactored: checkout button at bottom of menu

--- site/src/components/OrderButton.js
+++ site/src/components/OrderButton.js
@@ -5,10 +5,10 @@ import { Box, Flex, Icon, Text } from "@chakra-ui/core"
 const OrderButton = props => {
   return (
     <Box d="block" w="max-content" m="0 auto">
-      <Link to="/">
+      <Link to="/cart">
         <Flex flexDir="column" alignItems="center">
           <Text mb="1rem" fontSize="4xl" color="red.light" fontFamily="banner">
-            ÌPLACEèANèORDERÍ
+            ÌPROCEEDèTOèORDERÍ
           </Text>
           <Icon {...props} name="shop" color="white" />
         </Flex>

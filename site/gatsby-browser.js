fb5157ff393399350eb31ffec6447d7108a73656
XanderJL <alexanderjameslow@gmail.com>
2020-11-08 18:47:25 -0500

    maybe this time

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -9,8 +9,8 @@ module.exports.wrapRootElement = ({ element }) => {
   return (
     <CartProvider
       stripe={stripePromise}
-      successUrl={`${process.env.URL || "http://localhost:8000/success"}`}
-      cancelUrl={`${process.env.URL || "http://localhost:8000/cart"}`}
+      successUrl={`${process.env.GATSBY_PRODUCTION_URL || "http://localhost:8000/success"}`}
+      cancelUrl={`${process.env.GATSBY_PRODUCTION_URL || "http://localhost:8000/cart"}`}
       currency="CAD"
       allowedCountries={["CA"]}
       billingAddressCollection={true}

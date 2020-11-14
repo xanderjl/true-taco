0dced90e11c8bc51c5930b37c4953f5ef1671899
XanderJL <alexanderjameslow@gmail.com>
2020-11-14 13:04:29 -0500

    added: sanity deploy hook

---
+++ site/functions/sanity-deploy.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch")
+require("dotenv").config()
+
+exports.handler = async (event, context) => {
+  console.log(
+    `{\ncontext: ${JSON.stringify(context, null, 2)},\nevent: ${JSON.stringify(
+      event,
+      null,
+      2
+    )}\n}`
+  )
+  try {
+    await fetch("https://api.github.com/repos/XanderJL/last-draft/dispatches", {
+      method: "POST",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({ event_type: "sanity-deploy" }),
+    })
+  } catch (error) {
+    console.log(error)
+    return { statusCode: 500, body: "failed" }
+  }
+
+  return { statusCode: 200, body: "succeeded!" }
+}

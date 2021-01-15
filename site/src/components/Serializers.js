9fb334e56867a07c33890a02f611a3a48ef4b42e
Alex Low <me@alexlow.dev>
2021-01-14 20:39:51 -0500

    added: serializers for copy on the site

---
+++ site/src/components/Serializers.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Heading, Link, List, ListItem, Text } from "@chakra-ui/react"
+
+export default {
+  types: {
+    block: props => {
+      const { style = "normal" } = props.node
+
+      if (/^h\d/.test(style)) {
+        return (
+          <Heading as={style} fontFamily="body" fontSize="lg" pb="0.5rem">
+            {props.children}
+          </Heading>
+        )
+      }
+      return <Text pb="1rem">{props.children}</Text>
+    },
+  },
+  list: ({ children, options, type }) => {
+    return (
+      <List pb="1rem" styleType={type} stylePosition="inside">
+        {children}
+      </List>
+    )
+  },
+  listItem: ({ children }) => {
+    return <ListItem>{children}</ListItem>
+  },
+  marks: {
+    link: ({ mark, children }) => {
+      return (
+        <Link href={mark.href} color="green.400">
+          {children}
+        </Link>
+      )
+    },
+  },
+}

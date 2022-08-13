44f4f205e8016df94ca70057bc382a81ca8539b5
Xander Low <alexanderjameslow@gmail.com>
2022-08-13 13:14:44 -0700

    added: catering pdf

--- site/src/pages/catering.js
+++ site/src/pages/catering.js
@@ -1,10 +1,20 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import BlockContent from "@sanity/block-content-to-react"
-import { Box, Flex, Heading } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  Heading,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react"
 import Layout, { Container, Section } from "../components/Layout"
 import ContactForm from "../components/ContactForm"
 import FrillsBottom from "../images/frills/bottom.svg"
+import cateringMenu from "../assets/catering-menu.pdf"
+import { AiOutlineFilePdf } from "react-icons/ai"
 
 const Catering = () => {
   const data = useStaticQuery(graphql`
@@ -34,31 +44,35 @@ const Catering = () => {
         }}
       >
         <Container minH="40vh" pt={["3rem", "5rem"]}>
-          <Section>
+          <Section color="white">
             <Flex
-              w="100%"
-              h="100%"
               flexDir="column"
-              align="center"
               justify="center"
+              fontSize={["xl", "2xl"]}
+              maxW="70ch"
+              mx="auto"
             >
-              <Box pb="3rem">
-                <Heading
-                  as="h1"
-                  size="4xl"
-                  p="0.25rem 1.25rem"
-                  color="white"
-                  fontWeight="400"
-                  textAlign="center"
-                >
-                  {heading}
-                </Heading>
+              <Heading
+                as="h1"
+                size="4xl"
+                p="0.25rem 1.25rem"
+                pb="3rem"
+                fontWeight="400"
+                textAlign="center"
+              >
+                {heading}
+              </Heading>
+              <Box pb={8}>
+                <BlockContent blocks={_rawBody} />
               </Box>
-              <Box>
-                <Box maxW="70ch" color="white" fontSize={["xl", "2xl"]}>
-                  <BlockContent blocks={_rawBody} />
-                </Box>
-              </Box>
+              <List>
+                <Link href={cateringMenu}>
+                  <ListItem>
+                    <ListIcon as={AiOutlineFilePdf} color="whtie" />
+                    View our catering menu here
+                  </ListItem>
+                </Link>
+              </List>
             </Flex>
           </Section>
         </Container>
